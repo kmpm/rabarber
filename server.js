cc462436d24edfd80e0e60278dc6630334b837cd
Peter Magnusson <peter@birchroad.net>
2013-10-02 19:57:54 +0200

    Downgrade uid:gid after everything is up and listening

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 
-var lib = require('./lib');
-var app = require('./app');
+var lib = require('./lib'),
+  Pid = new require('./lib/pid'),
+  app = require('./app');
+
+var pid = new Pid(lib.config.get('pid'));
 
 app.listen(lib.config.get('PORT'), function(){
   console.log('http \tlistening on port', lib.config.get('PORT'));
+  pid.downgrade(function(p, g){
+    console.log("Now running as %s:%s", p, g);
+  });
 });
